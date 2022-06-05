e66cd900c15e7c61ab6d9a426a2fd08b4870124a
kazuma-desu <kmuthisha@gmail.com>
2022-06-05 19:56:14 +0530

    commit

--- app.py
+++ app.py
@@ -4,33 +4,34 @@ import json
 
 base_url = 'https://teileshop.diederichs.com'
 
-inputauto_url ="/index.php?action=article&manufacturer=32328&vehicle=698422"
-
 with open('lists.txt') as f:
     urls = f.readlines()
 
-class Item:
-    item_id: str
-    image_url: str
-    description: str
+for url1 in urls:
+    inputauto_url = url1.split('https://teileshop.diederichs.com', 1)[1]
 
-def getItemDetails(url: str):
-    r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
-    soup = BeautifulSoup(r.content, features="html.parser")
+    class Item:
+        item_id: str
+        image_url: str
+        description: str
 
-    item = Item()
+    def getItemDetails(url: str):
+        r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+        soup = BeautifulSoup(r.content, features="html.parser")
 
-    item.image_url = base_url + '/' + soup.find('img', id='zoomimage').get('src')
-    for td in soup.find_all(style='vertical-align:top'):
-        if td.get_text() == 'Item:':
-            item.item_id = td.next_sibling.get_text()
-        if td.get_text() == 'Description:':
-            item.description = td.next_sibling.get_text().replace('\t', '')
-    print("Fetched details for item " + item.item_id)
-    return item
+        item = Item()
 
-for url in urls:
-    vehicle_url = base_url + inputauto_url
+        item.image_url = base_url + '/' + soup.find('img', id='zoomimage').get('src')
+        for td in soup.find_all(style='vertical-align:top'):
+            if td.get_text() == 'Item:':
+                item.item_id = td.next_sibling.get_text()
+            if td.get_text() == 'Description:':
+                item.description = td.next_sibling.get_text().replace('\t', '')
+        print("Fetched details for item " + item.item_id)
+        return item
+
+
+    vehicle_url = url
     r = requests.get(vehicle_url, headers={'Accept-Language': 'en-US,en;q=0.5'})
     soup = BeautifulSoup(r.content, features="html.parser")
 
@@ -45,6 +46,3 @@ for url in urls:
 
     for itemobj in Items:
         print(json.dumps(itemobj.__dict__) + '\n\n')
-
-
-        

b6723adbb37cf7529895fc56e6ca7526a2717b73
LifeSanatizer <60814910+LifeSanatizer@users.noreply.github.com>
2022-06-05 19:11:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+base_url = 'https://teileshop.diederichs.com'
+
+inputauto_url ="/index.php?action=article&manufacturer=32328&vehicle=698422"
+
+class Item:
+    item_id: str
+    image_url: str
+    description: str
+
+def getItemDetails(url: str):
+    r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+    soup = BeautifulSoup(r.content, features="html.parser")
+
+    item = Item()
+
+    item.image_url = base_url + '/' + soup.find('img', id='zoomimage').get('src')
+    for td in soup.find_all(style='vertical-align:top'):
+        if td.get_text() == 'Item:':
+            item.item_id = td.next_sibling.get_text()
+        if td.get_text() == 'Description:':
+            item.description = td.next_sibling.get_text().replace('\t', '')
+    print("Fetched details for item " + item.item_id)
+    return item
+
+
+vehicle_url = base_url + inputauto_url
+r = requests.get(vehicle_url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+soup = BeautifulSoup(r.content, features="html.parser")
+
+items = soup.find_all('a', attrs={'popup': 'detail'})
+
+
+Items = []
+for item in items:
+    item_url = item.get('href')
+    itemDetails = getItemDetails(base_url + '/' + item_url + '&ajax=true')
+    Items.append(itemDetails)
+
+for itemobj in Items:
+  print(json.dumps(itemobj.__dict__) + '\n\n')
